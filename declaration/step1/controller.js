78f3fed851be62bb97146c6dfb3e1fb01f8a8696
TV <catindev@gmail.com>
2015-06-18 16:37:16 +0600

    ngRouter

--- declaration/step1/controller.js
+++ declaration/step1/controller.js
@@ -2,7 +2,7 @@ angular.module('declaration').controller('step1Cntrllr', [ '$scope', 'step1Lcl',
     var ctrl = this;
     ctrl.title = "ИИН ребёнка";
     ctrl.uin = "";
-    ctrl.state = "";
+    ctrl.state = "error";
 
     ctrl.uin_model = { value: '' };
 
